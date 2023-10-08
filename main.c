3c7adf409ac2b9c2065e6325ee9587fdf94df7e2
simobls <belassalsimo78@gmail.com>
2023-10-08 12:57:40 +0100

    added an infinite loop && a condition to exit

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
 #include "main.h"
 
 int main(){
-char *prompt = "(MyShell) $ ";
-char *line;
-size_t n =0;
+	char *prompt = "(MyShell) $ ";
+	char *line;
+	size_t n =0;
+	ssize_t n_char;
 
-	printf("%s", prompt);
-	getline(&line, &n, stdin);
+//infinite loop
+	while (true)
+	{
+		printf("%s", prompt);
+		// use ctrl-d to send an EOF - returns -1
+		n_char = getline(&line, &n, stdin);
 
-	free(line);
+// condition to exit MyShell - break the infinite loop
+
+		if (n_char == -1)
+		{
+			printf("Exit MyShell ...\n");
+			return (-1);
+		}
+
+		printf("%s", line);
+// free allocated memory
+		free(line);
+	}
+		
 return (0);
 }
