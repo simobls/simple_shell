b73766e3d618ec67ae583ec2ae0856380d59354d
simobls <belassalsimo78@gmail.com>
2023-10-11 12:28:59 +0100

    fixed the segmentation fault in the infinite loop

--- main.c
+++ main.c
@@ -3,71 +3,71 @@
 int main(int argc, char **argv){
 // declaration of variables
 	char *prompt = "(MyShell) $ ";
-	char *line = NULL, *line_copy = NULL, *token;
-	const char *delim = " \n";
-	size_t n = 0;
+	char *line;
+	size_t n =0;
 	int n_char;
-	int num_token;
+	const char* delim = " \n";
 
-// cast void on the arguments of the main
+//cast void on arguments
 	(void)argc, (void)argv;
 
-// infinite loop
-	while (true)
-	{
+//infinite loop
+	while (true){
+	//print the prompt
 		printf("%s", prompt);
 	// use ctrl-d to send an EOF - returns -1
 		n_char = getline(&line, &n, stdin);
 
 	// condition to exit MyShell - break the infinite loop
-		if (n_char == -1)
-		{
+		if (n_char == -1){
 			printf("Exit MyShell ...\n");
 			return (-1);
 		}
+		printf("command : %s", line);
 
-	//make a copy of the command after allocationg space
-		line_copy = malloc(sizeof(char)*n_char);
-		if (line_copy == NULL)
-		{
-			perror("tsh : memory allocation error\n");
+	// make a copy of the command after allocating the memory
+		char* line_copy = malloc(sizeof(char) * n_char);
+		if (line_copy == NULL){
+			perror("tsh : memory allocation error");
 			return (-1);
 		}
 		strcpy(line_copy, line);
-	
+		printf("copy of the command : %s\n", line_copy);
+
 	// parse - tokenize - the command we copied
-		token = strtok(line_copy, delim);
+		char *token = strtok(line_copy, delim);
 
-		// calculate the number of tokens
-		while (token != NULL)
-		{
-			num_token++;
-			token = strtok(NULL, delim); // we call strtok() with a NULL argument to make it continue tokenizing from it's previous position
+		// calculate number of tokens
+		int num_tokens = 0;
+		while (token != NULL){
+			num_tokens++;
+			token = strtok(NULL, delim);
 		}
-		num_token++;
+		printf("number of tokens : %d\n", num_tokens);
 
+		//storing the tokens
+		char **argv = (char **)malloc(num_tokens * sizeof(char *));
+		line_copy = strdup(line);
 		token = strtok(line_copy, delim);
 
 		int i = 0;
-		while (token != NULL)
-		{
-			argv[i] = malloc(sizeof(char)*strlen(token));
-			strcpy(argv[i], token);
-			i++;
-
+		while (token != NULL){
+			argv[i] = strdup(token); // Copy each token to the array
 			token = strtok(NULL, delim);
+			i++;
 		}
-		argv[i] = NULL;
-
-		for (i = 0; i < num_token; i++)
-		{
-			printf("%s\n", argv[i]);
+		//print the tokens
+		for (i = 0; i < num_tokens; i++){
+			printf("token %d : %s\n", i, argv[i]);
 		}
-
-	// free allocated memory
+		// free memory for the command copy and the tokens we made
+		for (i = 0; i < num_tokens; i++){
+			free(argv[i]);
+		}
 		free(argv);
 		free(line_copy);
 	}
+	// free allocated memory for the command
 	free(line);
 	return (0);
 }
