b9fe7811a80f8251a13b932811fcecbfe31d4133
simobls <138694317+simobls@users.noreply.github.com>
2023-10-18 00:23:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,41 @@
-#include "main.h"
-
-int main(int argc, char **argv)
-{
-	char *prompt = "(MyShell) $ ", *line; /* declaration of variables */
-	size_t n = 0;
-	int n_char;
-	const char *delim = " \n";
-	(void)argc, (void)argv; /* cast void on arguments */
-
-	while (true) /* infinite loop */
-	{
-		printf("%s", prompt); /* print the prompt */
-		n_char = getline(&line, &n, stdin); /* use ctrl-d to send an EOF */
-		if (n_char == -1) /* condition to exit MyShell - break the infinite loop */
-		{
-			printf("Exit MyShell ...\n");
-			return (-1);
-		}
-		printf("command : %s", line); // can be removed -- maybe
-		char *line_copy = malloc(sizeof(char) * n_char);
-
-		if (line_copy == NULL)
-		{
-			perror("tsh : memory allocation error");
-			return (-1);
-		}
-		strcpy(line_copy, line);
-		printf("copy of the command : %s\n", line_copy); //can be removed
-
-	/* parse - tokenize - the command we copied */
-		int num_tokens = CopyTokens(line_copy, delim, argv); /*calculate number of tokens*/
-
-		if (num_tokens > 0)
-		{
-			if (strcmp(argv[0], "exit") == 0) {
-                // Handle the 'exit' command
-                exit(0);
-			} else if (strcmp(argv[0], "cd") == 0)
-			{
-				// Handle the 'cd' command
-				// code
-			} else
-			{
-				execute_command(argv);
-			}
-		}
-		//printf("number of tokens : %d\n", num_tokens); //can be removed
-
-		//for (int i = 0; i < num_tokens; i++) /* print the tokens */ //can be removed
-			//printf("token %d : %s\n", i+1, argv[i]);
-	}
-	free(line); /* free allocated memory for the command */
-	return (0);
-}
+#include "shell.h" 
+  
+ /** 
+  * main - Entry point for a simple shell program. 
+  * @ac: The argument count. 
+  * @argv: The argument vector. 
+  * 
+  * Return: Always 0 (success). 
+  */ 
+ int main(int ac, char **argv) 
+ { 
+         char *line = NULL; 
+         char **command = NULL; 
+         int status = 0, idx = 0; 
+         (void)ac; 
+  
+         while (1) 
+         { 
+                 line = read_the_line(); 
+                 if (line == NULL) 
+                 { 
+                         /* Reached EOF (Ctrl + D) */ 
+                         if (isatty(STDIN_FILENO)) 
+                         { 
+                                 write(STDOUT_FILENO, "\n", 1); 
+                         } 
+                         return (status); 
+                 } 
+                 idx++; 
+  
+                 command = tokenizer(line); 
+                 if (!command) 
+                 { 
+                         continue; 
+                 } 
+                 if (is_builtin(command[0])) 
+                         handle_builtin(command, argv, &status, idx); 
+                 else 
+                         status = execute_(command, argv, idx); 
+         } 
+ }
